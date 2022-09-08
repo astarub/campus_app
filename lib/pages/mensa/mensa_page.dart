3b0f0367f8482b26010a33aa82f66fce05e95a3d
C4s4r <maik.hagelueken@asta-bochum.de>
2022-09-08 19:16:18 +0200

    Header with day selection
    This commit adds the header for the mensa-ui and a widget that allows the user to select a weekday.

--- lib/pages/mensa/mensa_page.dart
+++ lib/pages/mensa/mensa_page.dart
@@ -3,8 +3,9 @@ import 'package:provider/provider.dart';
 
 import 'package:campus_app/core/themes.dart';
 import 'package:campus_app/pages/home/widgets/page_navigation_animation.dart';
+import 'package:campus_app/pages/mensa/widgets/day_selection.dart';
 
-class MensaPage extends StatelessWidget {
+class MensaPage extends StatefulWidget {
   final GlobalKey<AnimatedEntryState> pageEntryAnimationKey;
   final GlobalKey<AnimatedExitState> pageExitAnimationKey;
 
@@ -15,15 +16,44 @@ class MensaPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<MensaPage> createState() => _MensaPageState();
+}
+
+class _MensaPageState extends State<MensaPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Provider.of<ThemesNotifier>(context).currentThemeData.backgroundColor,
       body: Center(
         child: AnimatedExit(
-          key: pageExitAnimationKey,
+          key: widget.pageExitAnimationKey,
           child: AnimatedEntry(
-            key: pageEntryAnimationKey,
-            child: const Text('Mensa', style: TextStyle(fontFamily: 'CircularStd', fontSize: 24)),
+            key: widget.pageEntryAnimationKey,
+            child: Column(
+              children: [
+                // Header
+                Container(
+                  padding: const EdgeInsets.only(top: 40, bottom: 20),
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      // Title
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 24),
+                        child: Text(
+                          'Events',
+                          style: Provider.of<ThemesNotifier>(context).currentThemeData.textTheme.displayMedium,
+                        ),
+                      ),
+                      // Day selection
+                      MensaDaySelection(onChanged: (int _) {}),
+                    ],
+                  ),
+                ),
+                // Place expandables
+                //ListView(),
+              ],
+            ),
           ),
         ),
       ),
