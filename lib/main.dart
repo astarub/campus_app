5afa5215583b20b1109c6eb2c7a50466a11696c3
Domai <tim.barsch@rub.de>
2022-10-02 12:26:48 +0200

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 
 import 'package:campus_app/core/authentication/authentication_handler.dart';
 import 'package:campus_app/core/injection.dart' as ic; // injection container
+import 'package:campus_app/core/settings.dart';
 import 'package:campus_app/core/themes.dart';
 import 'package:campus_app/pages/rubnews/news_entity.dart';
 import 'package:campus_app/pages/splash/splash_page.dart';
