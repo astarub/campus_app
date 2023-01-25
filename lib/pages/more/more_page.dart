5d9ee7ed57900fcb07d701b614f58b6ff397a40d
henry-herrmann <henryherrmann.tv@gmail.com>
2023-01-25 18:38:30 +0100

    Add tranzkreis to the more page

--- lib/pages/more/more_page.dart
+++ lib/pages/more/more_page.dart
@@ -92,6 +92,11 @@ class MorePage extends StatelessWidget {
                             leadingIconPath: 'assets/img/asta_logo.png',
                             onTap: () => openLink(context, 'https://asta-bochum.de/repair-cafe/'),
                           ),
+                          ExternalLinkButton(
+                            title: 'Tanzkreis',
+                            leadingIconPath: 'assets/img/asta_logo.png',
+                            onTap: () => openLink(context, 'https://asta-bochum.de/tanzkreis/'),
+                          ),
                           Flex(
                             direction: Axis.horizontal,
                             children: [
