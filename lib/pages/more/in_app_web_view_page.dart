82ed29a0b52900f2fe18515d4dba32e81a13f295
henry-herrmann <henryherrmann.tv@gmail.com>
2023-03-05 09:38:03 +0100

    Fix bad performance on mensa page & in app browser scrolling bug
    This commit fixes the bad performance upon loading the mensa page caused by the amount of widgets of the Kulturcafe being loaded into the pipeline.

--- lib/pages/more/in_app_web_view_page.dart
+++ lib/pages/more/in_app_web_view_page.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -73,6 +75,7 @@ class _InAppWebViewPageState extends State<InAppWebViewPage> {
           child: Stack(
             children: [
               InAppWebView(
+                gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{}..add(const Factory<VerticalDragGestureRecognizer>(VerticalDragGestureRecognizer.new)),
                 pullToRefreshController: pullToRefreshController,
                 initialOptions: options,
                 initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
