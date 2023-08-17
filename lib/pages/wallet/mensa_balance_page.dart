95a921aa7245bb556752219a43b96f4c83f4f798
MixColumns <134201910+MixColumns@users.noreply.github.com>
2023-08-17 15:42:14 +0200

    small syntax fix

--- lib/pages/wallet/mensa_balance_page.dart
+++ lib/pages/wallet/mensa_balance_page.dart
@@ -33,7 +33,8 @@ class _MensaBalancePageState extends State<MensaBalancePage> with TickerProvider
         .copyWith(lastMensaBalance: scannedBalance, lastMensaTransaction: lastTransaction);
 
     debugPrint(
-        'Saving scanned mensa card data: Balance=${newSettings.lastMensaBalance}, Last Transaction: ${newSettings.lastMensaTransaction}');
+      'Saving scanned mensa card data: Balance=${newSettings.lastMensaBalance}, Last Transaction: ${newSettings.lastMensaTransaction}',
+    );
     Provider.of<SettingsHandler>(context, listen: false).currentSettings = newSettings;
   }
 
@@ -51,7 +52,8 @@ class _MensaBalancePageState extends State<MensaBalancePage> with TickerProvider
       // Select application
       await FlutterNfcKit.transceive(
         Uint8List.fromList(
-            [0x90, 0x5A, 0x00, 0x00, 3, (0x5F8415 & 0xFF0000) >> 16, (0x5F8415 & 0xFF00) >> 8, 0x5F8415 & 0xFF, 0x00]),
+          [0x90, 0x5A, 0x00, 0x00, 3, (0x5F8415 & 0xFF0000) >> 16, (0x5F8415 & 0xFF00) >> 8, 0x5F8415 & 0xFF, 0x00],
+        ),
       );
 
       // Get the transaction history file
