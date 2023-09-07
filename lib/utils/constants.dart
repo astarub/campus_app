f3a939417092b6c5779682781d2731480424466e
Henry Herrmann <henryherrmann.tv@gmail.com>
2023-09-07 17:50:11 +0200

    Update Backend URL & Add Backend Status Badge

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,9 +10,9 @@ const String appEvents = '$appDataHost/wp-json/tribe/events/v1/events';
 const String appFeed = '$appDataHost/wp-json/wp/v2/posts';
 const String rubNewsfeed = 'https://news.rub.de/newsfeed'; // there is no non-german
 
-const String appwrite = 'https://api-app2.asta-bochum.de/v1';
+const String appwrite = 'https://api-app.asta-bochum.de/v1';
 
-const String mensaData = 'https://api-app2.asta-bochum.de/get_meal';
+const String mensaData = 'https://api-app.asta-bochum.de/get_meal';
 
 // See: https://codewithandrea.com/articles/flutter-api-keys-dart-define-env-files/
 final String mensaApiKey = Env.mensaApiKey;
