fff5e998b39e28f8519a710254eb460cbad86fd0
Domai 🌳 <75212885+domai-tb@users.noreply.github.com>
2025-04-16 10:44:52 +0200

    Dependency Upgrade (#193)
    * upgrade AGP
    * upgrade packages
    * regenerate mocks
    * handle analyze options
    * fix package upgrades manually
    * do not use synthetic package
    * remove duplicated sentry dependency

--- lib/l10n/l10n_en.dart
+++ lib/l10n/l10n_en.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'l10n.dart';
 
-import 'package:campus_app/l10n/l10n.dart';
+// ignore_for_file: type=lint
 
 /// The translations for English (`en`).
 class AppLocalizationsEn extends AppLocalizations {
-  AppLocalizationsEn([super.locale = 'en']);
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
   String get helloWorld => 'Hello World!';
