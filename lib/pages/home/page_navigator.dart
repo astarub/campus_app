bb6f08f9846cb441cc79606455b0da2a8fabf7b8
Domai <github@domai-tb.com>
2025-08-03 14:42:22 +0200

    re-add feedpage

--- lib/pages/home/page_navigator.dart
+++ lib/pages/home/page_navigator.dart
@@ -1,24 +1,11 @@
-import 'package:flutter/material.dart';
-
-//import 'package:campus_app/pages/feed/feed_page.dart';
+import 'package:campus_app/pages/feed/feed_page.dart';
 import 'package:campus_app/pages/home/widgets/bottom_nav_bar.dart';
-import 'package:campus_app/pages/feed/calendar/calendar_page.dart';
-import 'package:campus_app/pages/mensa/mensa_page.dart';
-import 'package:campus_app/pages/wallet/wallet_page.dart';
-import 'package:campus_app/pages/more/more_page.dart';
 import 'package:campus_app/pages/home/widgets/page_navigation_animation.dart';
+import 'package:campus_app/pages/mensa/mensa_page.dart';
+import 'package:campus_app/pages/more/more_page.dart';
 import 'package:campus_app/pages/planner/planner_page.dart';
-
-enum PageItem { feed, events, coupons, mensa, wallet, more }
-
-class PageNavigatorRoutes {
-  /// The root-page is shown initially when this navbar-tab is the active one.
-  static const String root = '/';
-
-  /// The detail-page is pushed onto the navigator-stack of this specific tab when,
-  /// for example, a news-article is opened.
-  static const String detail = '/detail';
-}
+import 'package:campus_app/pages/wallet/wallet_page.dart';
+import 'package:flutter/material.dart';
 
 /// Wraps the displayed page into a seperate [Navigator] in order to push new detail-pages
 /// (like opening a news-article) to a specific navigator-stack instead of the app-wide navigator-stack.
@@ -49,17 +36,27 @@ class NavBarNavigator extends StatelessWidget {
     required this.pageExitAnimationKey,
   });
 
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, WidgetBuilder> routeBuilders = _routeBuilders(context);
+
+    return Navigator(
+      key: navigatorKey,
+      initialRoute: PageNavigatorRoutes.root,
+      onGenerateRoute: (routeSettings) {
+        return MaterialPageRoute(
+          builder: (context) => routeBuilders[routeSettings.name]!(context),
+        );
+      },
+    );
+  }
+
   /// Creates a map of the root and detail page of the specific page.
   Map<String, WidgetBuilder> _routeBuilders(BuildContext context) {
     Widget rootPage;
     switch (pageItem) {
       case PageItem.feed:
-        /* rootPage = FeedPage(
-          mainNavigatorKey: mainNavigatorKey,
-          pageEntryAnimationKey: pageEntryAnimationKey,
-          pageExitAnimationKey: pageExitAnimationKey,
-        ); */
-        rootPage = CalendarPage(
+        rootPage = FeedPage(
           mainNavigatorKey: mainNavigatorKey,
           pageEntryAnimationKey: pageEntryAnimationKey,
           pageExitAnimationKey: pageExitAnimationKey,
@@ -101,19 +98,15 @@ class NavBarNavigator extends StatelessWidget {
       //TabNavigatorRoutes.detail: (context) => ,
     };
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    final Map<String, WidgetBuilder> routeBuilders = _routeBuilders(context);
+enum PageItem { feed, events, coupons, mensa, wallet, more }
 
-    return Navigator(
-      key: navigatorKey,
-      initialRoute: PageNavigatorRoutes.root,
-      onGenerateRoute: (routeSettings) {
-        return MaterialPageRoute(
-          builder: (context) => routeBuilders[routeSettings.name]!(context),
-        );
-      },
-    );
-  }
+class PageNavigatorRoutes {
+  /// The root-page is shown initially when this navbar-tab is the active one.
+  static const String root = '/';
+
+  /// The detail-page is pushed onto the navigator-stack of this specific tab when,
+  /// for example, a news-article is opened.
+  static const String detail = '/detail';
 }
