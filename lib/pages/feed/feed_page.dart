eb82dedb2f3c6e134c054ac4150a054cc8c222a1
Domai <github@domai-tb.com>
2025-08-03 16:05:33 +0200

    call event usecase from feed page

--- lib/pages/feed/feed_page.dart
+++ lib/pages/feed/feed_page.dart
@@ -1,25 +1,26 @@
 import 'dart:io' show Platform;
-import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-import 'package:snapping_sheet_2/snapping_sheet.dart';
-
-import 'package:campus_app/core/failures.dart';
-import 'package:campus_app/core/injection.dart';
-import 'package:campus_app/core/themes.dart';
-import 'package:campus_app/core/settings.dart';
 import 'package:campus_app/core/backend/backend_repository.dart';
 import 'package:campus_app/core/backend/entities/publisher_entity.dart';
+import 'package:campus_app/core/failures.dart';
+import 'package:campus_app/core/injection.dart';
+import 'package:campus_app/core/settings.dart';
+import 'package:campus_app/core/themes.dart';
+import 'package:campus_app/pages/feed/calendar/calendar_usecases.dart';
+import 'package:campus_app/pages/feed/calendar/entities/event_entity.dart';
 import 'package:campus_app/pages/feed/news/news_entity.dart';
 import 'package:campus_app/pages/feed/news/news_usecases.dart';
-import 'package:campus_app/pages/feed/widgets/feed_item.dart';
 import 'package:campus_app/pages/feed/widgets/feed_filter_popup.dart';
+import 'package:campus_app/pages/feed/widgets/feed_item.dart';
 import 'package:campus_app/pages/home/widgets/page_navigation_animation.dart';
 import 'package:campus_app/utils/pages/feed_utils.dart';
 import 'package:campus_app/utils/widgets/campus_icon_button.dart';
-import 'package:campus_app/utils/widgets/campus_segmented_control.dart';
 import 'package:campus_app/utils/widgets/campus_search_bar.dart';
+import 'package:campus_app/utils/widgets/campus_segmented_control.dart';
 import 'package:campus_app/utils/widgets/scroll_to_top_button.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snapping_sheet_2/snapping_sheet.dart';
 
 class FeedPage extends StatefulWidget {
   final GlobalKey<NavigatorState> mainNavigatorKey;
@@ -46,12 +47,16 @@ class FeedPageState extends State<FeedPage> with WidgetsBindingObserver, Automat
   double newsWidgetOpacity = 1;
 
   List<NewsEntity> news = [];
+  List<Event> events = [];
   List<Failure> failures = [];
-  List<Widget> parsedNewsWidgets = [];
-  List<Widget> searchNewsWidgets = [];
 
-  final NewsUsecases _newsUsecases = sl<NewsUsecases>();
-  final FeedUtils _feedUtils = sl<FeedUtils>();
+  List<Widget> parsedFeedWidgets = [];
+  List<Widget> searchFeedWidgets = [];
+
+  final NewsUsecases newsUsecases = sl<NewsUsecases>();
+  final CalendarUsecases calendarUsecases = sl<CalendarUsecases>();
+
+  final FeedUtils feedUtils = sl<FeedUtils>();
   final BackendRepository backendRepository = sl<BackendRepository>();
 
   late final SnappingSheetController popupController;
@@ -59,133 +64,9 @@ class FeedPageState extends State<FeedPage> with WidgetsBindingObserver, Automat
   String searchWord = '';
   bool showSearchBar = false;
 
-  /// Function that call usecase and parse widgets into the corresponding
-  /// lists of events, news and failures.
-  Future<void> updateStateWithFeed({bool withAnimation = false}) async {
-    if (withAnimation) setState(() => newsWidgetOpacity = 0);
-
-    try {
-      await backendRepository.loadPublishers(Provider.of<SettingsHandler>(context, listen: false));
-      // ignore: empty_catches
-    } catch (e) {}
-
-    // TODO: Add calendar entries to feed
-    final newsData = await _newsUsecases.updateFeedAndFailures();
-
-    try {
-      setState(() {
-        // TODO: Do the same for event data
-        news = newsData['news'] != null ? newsData['news']! as List<NewsEntity> : [];
-        parsedNewsWidgets = parseUpdateToWidgets();
-      });
-    } catch (e) {
-      debugPrint('Error: $e');
-    }
-
-    // Apply search to newly parsed feed items
-    onSearch(searchWord);
-
-    debugPrint('Feed aktualisiert.');
-  }
-
-  /// Parse the updated news data into widgets and mix them with events if needed
-  List<Widget> parseUpdateToWidgets() {
-    setState(() => newsWidgetOpacity = 1);
-
-    return _feedUtils.fromEntitiesToWidgetList(
-      news: news,
-    );
-  }
-
-  void saveChangedFilters(List<Publisher> newFilters) {
-    final Settings newSettings =
-        Provider.of<SettingsHandler>(context, listen: false).currentSettings.copyWith(feedFilter: newFilters);
-
-    debugPrint('Saving new feed filters: ${newSettings.feedFilter.map((e) => e.name).toList()}');
-    Provider.of<SettingsHandler>(context, listen: false).currentSettings = newSettings;
-  }
-
-  void saveFeedExplore(int selected) {
-    bool explore = false;
-    if (selected == 1) explore = true;
-
-    final Settings newSettings =
-        Provider.of<SettingsHandler>(context, listen: false).currentSettings.copyWith(newsExplore: explore);
-
-    debugPrint('Saving newsExplore: ${newSettings.newsExplore}');
-    Provider.of<SettingsHandler>(context, listen: false).currentSettings = newSettings;
-
-    // Mix in widget when changed to the explore section and vice versa
-    setState(() {
-      parsedNewsWidgets = parseUpdateToWidgets();
-      onSearch(searchWord);
-    });
-  }
-
-  /// Filters the feed based on the search input of the user
-  void onSearch(String search) {
-    final List<Widget> filteredWidgets = [];
-
-    for (final Widget e in parsedNewsWidgets) {
-      // TODO: Add case for EventEntity
-      if (e is FeedItem) {
-        if (e.title.toUpperCase().contains(search.toUpperCase())) {
-          filteredWidgets.add(e);
-        }
-      } else {
-        filteredWidgets.add(e);
-      }
-    }
-
-    setState(() {
-      searchNewsWidgets = filteredWidgets;
-      searchWord = search;
-    });
-  }
-
+  // Keep state alive
   @override
-  void initState() {
-    super.initState();
-
-    // Add observer in order to listen to `didChangeAppLifecycleState`
-    WidgetsBinding.instance.addObserver(this);
-
-    _scrollController = ScrollController()
-      ..addListener(() {
-        if (_scrollController.offset > (scrollControllerLastOffset + 80) && _scrollController.offset > 0) {
-          scrollControllerLastOffset = _scrollController.offset;
-          if (headerOpacity != 0) setState(() => headerOpacity = 0);
-        } else if (_scrollController.offset < (scrollControllerLastOffset - 250)) {
-          scrollControllerLastOffset = _scrollController.offset;
-          if (headerOpacity != 1) setState(() => headerOpacity = 1);
-        } else if (_scrollController.offset < 80) {
-          scrollControllerLastOffset = 0;
-          if (headerOpacity != 1) setState(() => headerOpacity = 1);
-        }
-      });
-
-    popupController = SnappingSheetController();
-
-    // initial data request
-    final newsData = _newsUsecases.getCachedFeedAndFailures();
-    news = newsData['news']! as List<NewsEntity>; // empty when no data was cached before
-    failures = newsData['failures']! as List<Failure>; // CachFailure when no data was cached before
-
-    // Request an update for the feed and show the refresh indicator
-    Future.delayed(const Duration(milliseconds: 200)).then((_) {
-      refreshIndicatorKey.currentState?.show();
-    });
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    // Refresh feed data when app gets back into foreground
-    if (state == AppLifecycleState.resumed) {
-      updateStateWithFeed();
-    }
-  }
+  bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
@@ -196,11 +77,11 @@ class FeedPageState extends State<FeedPage> with WidgetsBindingObserver, Automat
     final explore = Provider.of<SettingsHandler>(context, listen: false).currentSettings.newsExplore;
 
     final List<Widget> filteredFeedItems = !explore
-        ? _feedUtils.filterFeedWidgets(
+        ? feedUtils.filterFeedWidgets(
             filters,
-            searchWord != '' ? searchNewsWidgets : parsedNewsWidgets,
+            searchWord != '' ? searchFeedWidgets : parsedFeedWidgets,
           )
-        : (searchWord != '' ? searchNewsWidgets : parsedNewsWidgets);
+        : (searchWord != '' ? searchFeedWidgets : parsedFeedWidgets);
 
     return Scaffold(
       backgroundColor: Provider.of<ThemesNotifier>(context).currentThemeData.colorScheme.surface,
@@ -268,7 +149,7 @@ class FeedPageState extends State<FeedPage> with WidgetsBindingObserver, Automat
                                   onChange: onSearch,
                                   onBack: () {
                                     setState(() {
-                                      searchNewsWidgets = parsedNewsWidgets;
+                                      searchFeedWidgets = parsedFeedWidgets;
                                       showSearchBar = false;
                                       searchWord = '';
                                     });
@@ -336,7 +217,129 @@ class FeedPageState extends State<FeedPage> with WidgetsBindingObserver, Automat
     );
   }
 
-  // Keep state alive
   @override
-  bool get wantKeepAlive => true;
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    // Refresh feed data when app gets back into foreground
+    if (state == AppLifecycleState.resumed) {
+      updateStateWithFeed();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Add observer in order to listen to `didChangeAppLifecycleState`
+    WidgetsBinding.instance.addObserver(this);
+
+    _scrollController = ScrollController()
+      ..addListener(() {
+        if (_scrollController.offset > (scrollControllerLastOffset + 80) && _scrollController.offset > 0) {
+          scrollControllerLastOffset = _scrollController.offset;
+          if (headerOpacity != 0) setState(() => headerOpacity = 0);
+        } else if (_scrollController.offset < (scrollControllerLastOffset - 250)) {
+          scrollControllerLastOffset = _scrollController.offset;
+          if (headerOpacity != 1) setState(() => headerOpacity = 1);
+        } else if (_scrollController.offset < 80) {
+          scrollControllerLastOffset = 0;
+          if (headerOpacity != 1) setState(() => headerOpacity = 1);
+        }
+      });
+
+    popupController = SnappingSheetController();
+
+    // initial data request
+    final newsData = newsUsecases.getCachedFeedAndFailures();
+    news = newsData['news']! as List<NewsEntity>; // empty when no data was cached before
+    failures = newsData['failures']! as List<Failure>; // CachFailure when no data was cached before
+
+    // Request an update for the feed and show the refresh indicator
+    Future.delayed(const Duration(milliseconds: 200)).then((_) {
+      refreshIndicatorKey.currentState?.show();
+    });
+  }
+
+  /// Filters the feed based on the search input of the user
+  void onSearch(String search) {
+    final List<Widget> filteredWidgets = [];
+
+    for (final Widget e in parsedFeedWidgets) {
+      // TODO: Add case for EventEntity
+      if (e is FeedItem) {
+        if (e.title.toUpperCase().contains(search.toUpperCase())) {
+          filteredWidgets.add(e);
+        }
+      } else {
+        filteredWidgets.add(e);
+      }
+    }
+
+    setState(() {
+      searchFeedWidgets = filteredWidgets;
+      searchWord = search;
+    });
+  }
+
+  /// Parse the updated news data into widgets and mix them with events if needed
+  List<Widget> parseUpdateToWidgets() {
+    setState(() => newsWidgetOpacity = 1);
+
+    return feedUtils.fromEntitiesToWidgetList(news: news, events: events);
+  }
+
+  void saveChangedFilters(List<Publisher> newFilters) {
+    final Settings newSettings =
+        Provider.of<SettingsHandler>(context, listen: false).currentSettings.copyWith(feedFilter: newFilters);
+
+    debugPrint('Saving new feed filters: ${newSettings.feedFilter.map((e) => e.name).toList()}');
+    Provider.of<SettingsHandler>(context, listen: false).currentSettings = newSettings;
+  }
+
+  void saveFeedExplore(int selected) {
+    bool explore = false;
+    if (selected == 1) explore = true;
+
+    final Settings newSettings =
+        Provider.of<SettingsHandler>(context, listen: false).currentSettings.copyWith(newsExplore: explore);
+
+    debugPrint('Saving newsExplore: ${newSettings.newsExplore}');
+    Provider.of<SettingsHandler>(context, listen: false).currentSettings = newSettings;
+
+    // Mix in widget when changed to the explore section and vice versa
+    setState(() {
+      parsedFeedWidgets = parseUpdateToWidgets();
+      onSearch(searchWord);
+    });
+  }
+
+  /// Function that call usecase and parse widgets into the corresponding
+  /// lists of events, news and failures.
+  Future<void> updateStateWithFeed({bool withAnimation = false}) async {
+    if (withAnimation) setState(() => newsWidgetOpacity = 0);
+
+    try {
+      await backendRepository.loadPublishers(Provider.of<SettingsHandler>(context, listen: false));
+      // ignore: empty_catches
+    } catch (e) {}
+
+    final newsData = await newsUsecases.updateFeedAndFailures();
+    final eventData = await calendarUsecases.updateEventsAndFailures();
+
+    try {
+      setState(() {
+        news = newsData['news'] != null ? newsData['news']! as List<NewsEntity> : [];
+        events = eventData['events'] != null ? eventData['events']! as List<Event> : [];
+        parsedFeedWidgets = parseUpdateToWidgets();
+      });
+    } catch (e) {
+      debugPrint('Error: $e');
+    }
+
+    // Apply search to newly parsed feed items
+    onSearch(searchWord);
+
+    debugPrint('Feed aktualisiert.');
+  }
 }
--- lib/utils/pages/feed_utils.dart
+++ lib/utils/pages/feed_utils.dart
@@ -1,23 +1,51 @@
-import 'package:flutter/widgets.dart';
-
 import 'package:cached_network_image/cached_network_image.dart';
-
 import 'package:campus_app/core/backend/entities/publisher_entity.dart';
+import 'package:campus_app/pages/feed/calendar/entities/event_entity.dart';
 import 'package:campus_app/pages/feed/news/news_entity.dart';
 import 'package:campus_app/pages/feed/widgets/feed_item.dart';
 import 'package:campus_app/utils/constants.dart';
+import 'package:flutter/widgets.dart';
 
 class FeedUtils {
   // Save the shuffeled list to prevent constant re-shuffeling
   List shuffeledItemOrEventWidgets = [];
 
+  List<Widget> filterFeedWidgets(List<Publisher> filters, List<Widget> parsedFeedItems) {
+    final List<Widget> filteredFeedItems = [];
+
+    final List<String> filterNames = filters.map((e) => e.name).toList();
+
+    for (final Widget f in parsedFeedItems) {
+      if (f is FeedItem) {
+        if (f.link.startsWith('https://news.rub.de') && filterNames.contains('RUB')) {
+          filteredFeedItems.add(f);
+        }
+        if (f.link.startsWith('https://asta-bochum.de') && filterNames.contains('AStA')) {
+          filteredFeedItems.add(f);
+        }
+
+        if (f.link.startsWith(appWordpressHost) && (f.author != 0 && filters.map((e) => e.id).contains(f.author)) ||
+            f.categoryIds.contains(66)) {
+          filteredFeedItems.add(f);
+        }
+      } else {
+        filteredFeedItems.add(f);
+      }
+    }
+
+    return filteredFeedItems;
+  }
+
   /// Parse a list of NewsEntity and a list of Events to a widget list of type FeedItem sorted by date.
   /// For Padding insert at first position a SizedBox with heigth := 80 or given heigth.
   List<Widget> fromEntitiesToWidgetList({
     required List<NewsEntity> news,
+    required List<Event> events,
     double? heigth,
     bool shuffle = false,
   }) {
+    // TODO: Add Events into feed (the old implementation doesn't match the current items / widgest)
+
     List<dynamic> feedItemOrEventWidget = <dynamic>[];
 
     List<Widget> widgets = <Widget>[];
@@ -113,43 +141,17 @@ class FeedUtils {
     return widgets;
   }
 
-  List<Widget> filterFeedWidgets(List<Publisher> filters, List<Widget> parsedFeedItems) {
-    final List<Widget> filteredFeedItems = [];
-
-    final List<String> filterNames = filters.map((e) => e.name).toList();
-
-    for (final Widget f in parsedFeedItems) {
-      if (f is FeedItem) {
-        if (f.link.startsWith('https://news.rub.de') && filterNames.contains('RUB')) {
-          filteredFeedItems.add(f);
-        }
-        if (f.link.startsWith('https://asta-bochum.de') && filterNames.contains('AStA')) {
-          filteredFeedItems.add(f);
-        }
-
-        if (f.link.startsWith(appWordpressHost) && (f.author != 0 && filters.map((e) => e.id).contains(f.author)) ||
-            f.categoryIds.contains(66)) {
-          filteredFeedItems.add(f);
-        }
-      } else {
-        filteredFeedItems.add(f);
-      }
-    }
-
-    return filteredFeedItems;
-  }
-
-  int sortFeedDesc(dynamic a, dynamic b) {
+  int sortFeedAsc(dynamic a, dynamic b) {
     if (a is FeedItem && b is FeedItem) {
-      return b.date.compareTo(a.date);
+      return a.date.compareTo(b.date);
     } else {
       return 0;
     }
   }
 
-  int sortFeedAsc(dynamic a, dynamic b) {
+  int sortFeedDesc(dynamic a, dynamic b) {
     if (a is FeedItem && b is FeedItem) {
-      return a.date.compareTo(b.date);
+      return b.date.compareTo(a.date);
     } else {
       return 0;
     }
